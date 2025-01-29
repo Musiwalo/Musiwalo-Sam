128662a05407330406e0415a8faf87505c29dfe3
Mulaudzi Musiwalo <126598211+Musiwalo@users.noreply.github.com>
2025-01-29 13:56:16 +0200

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 29 10:36:20 2025
+
+@author: mulau
+"""
+
+import streamlit as st
+st.write("Hello, Streamlit!")
+
+st.write("This is my firsy web app")
+
+st.header ("Sample data")
+
